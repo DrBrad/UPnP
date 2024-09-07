b5f2a4b4c9599c6f5e15d3a2bde7dd7d11365a71
DrBrad <brad@bradeagle.com>
2024-09-06 21:54:01 -0600

    Fix to test cases

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ mod tests {
     use crate::upnp::upnp::UPnP;
 
     #[test]
-    fn external_ip() {
+    fn test() {
         let upnp = UPnP::new(IpAddr::from([192, 168, 0, 129])).expect("Cannot find gateway");
         println!("{}", upnp.get_external_ip().unwrap().to_string());
         println!("OPEN: {:?}", upnp.open_port(4040, Tcp).unwrap());
